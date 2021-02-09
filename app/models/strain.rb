86c99919f41b51198ea8fcf7370ee36bf705df05
Lina Vallejo <linav92@gmail.com>
2021-02-08 23:27:53 -0300

    Rspec model final

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,4 +1,5 @@
 class Strain < ApplicationRecord
     has_many :assemblies,  dependent: :destroy 
     has_many :wines, through: :assemblies,  dependent: :destroy
+    validates :name, presence: true
 end
--- spec/models/strain_spec.rb
+++ spec/models/strain_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Strain, type: :model do
-  it 'validates uniqueness name'
+  describe "Las cepas no pueden tener el mismo nombre" do 
+    it 'validates uniqueness name' do
+      expect(Strain.validates_uniqueness_of(:name))
+    end
+  end
+
+  describe "Una cepa no puede tener un nombre vacío" do 
+    it 'validates name not empty' do
+      strain  = Strain.new(name: "")
+      expect(strain).to_not be_valid
+    end
+  end
+
+  describe "Una cepa no puede tener un nombre vacío" do 
+    it 'validates name not nil' do
+      strain  = Strain.new(name: nil)
+      expect(strain).to_not be_valid
+    end
+  end
+
+  describe "Una cepa no puede tener un nombre vacío" do 
+    it 'validates name carmenere' do
+      strain  = Strain.new(name: "Carmenere")
+      expect(strain).to be 
+    end
+  end
+ 
 end
